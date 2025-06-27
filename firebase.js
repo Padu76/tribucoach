7c4996e1992ae28c1d7cfe0a5aca7bddbd27d16b
Padu76 <andrea.padoan@gmail.com>
2025-06-28 00:31:17 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+// Se intendi usare Analytics, DEVI importare getAnalytics
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDSsQEPii_Eb99cWGs7eqozgTtIqbtO2rs",
@@ -9,15 +11,18 @@ const firebaseConfig = {
   projectId: "tribucoach-9564b",
   storageBucket: "tribucoach-9564b.firebasestorage.app",
   messagingSenderId: "476626832985",
-  appId: "1:476626832985:web:7835188f40f4348567ab48"
-  // RIMOSSO measurementId per evitare conflitti Analytics
+  appId: "1:476626832985:web:7835188f40f4348567ab48",
+  // AGGIUNGI O AGGIORNA QUESTO CAMPO con il Measurement ID corretto
+  measurementId: "G-EVYE5YBZJE"
 };
 
 // Inizializza Firebase
 const app = initializeApp(firebaseConfig);
 
-// Esporta i servizi Firebase (SENZA Analytics)
+// Esporta i servizi Firebase
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+// Esporta anche Analytics se lo stai usando
+export const analytics = getAnalytics(app);
 
 export default app;
